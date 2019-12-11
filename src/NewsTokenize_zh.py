58399d8d3e93a3eb43779c76d782be5b4d7b0de0
Leo T. Chen <leotchen@deloitte.com.tw>
2019-12-11 10:36:17 +0800

    update new key words algorithm & filter out not necessary words

--- src/NewsTokenize_zh.py
+++ src/NewsTokenize_zh.py
@@ -4,12 +4,14 @@ import pandas as pd
 from collections import Counter
 from sklearn.feature_extraction.text import *
 from summa import keywords
+import time
 
+load_start = time.time()
 # data_utils.download_data_gdown("../Model/") # only first time
 pre = "D:\python\LEO_TM\BERT_chinese_LM_processing\src\\"  # for pycharm only
 define_dict_path = pre + "../data/TextTokenize_zh/company_dict.txt"
-news_path = pre + "../data/TextTokenize_zh/POC_NEWS.csv"
-result_path = pre + "../Results/tokenize_news_zh_Leo.xlsx"
+news_path = pre + "../data/TextTokenize_zh/Foxconn_News_2018.csv"  # POC_NEWS.csv
+result_path = pre + "../Results/tokenize_Foxconn2018_news_Leo.xlsx"
 
 ws = WS(pre + "../Model/data")
 pos = POS(pre + "../Model/data")
@@ -17,31 +19,43 @@ ner = NER(pre + "../Model/data")
 
 word_to_weight = {}
 with open(define_dict_path, "r", encoding='utf8') as file:
-    for line in tqdm(file):
+    for line in file:
         key, value = line.split()
         word_to_weight[str(key)] = 2
 dictionary = construct_dictionary(word_to_weight)
 # print(dictionary)  # dict
+load_end = time.time() - load_start
+tokenize_start = time.time()
 
 news_df = pd.read_csv(news_path, encoding='cp950')
 all_news_li = news_df.內容.tolist()
 all_title_li = news_df.標題.tolist()
 all_news_li2 = []
 for title, news in zip(all_title_li, all_news_li):
-    all_news_li2.append(title + "：" + news)
+    if type(news) == float:  # news is nan, only title
+        all_news_li2.append(title)
+    elif type(title) == float:
+        all_news_li2.append(news)
+    else:
+        all_news_li2.append(title + "：" + news)
 
-word_sentence_list = ws(all_news_li2, recommend_dictionary=dictionary, segment_delimiter_set={",", "。", ":", "?", "!", ";", "，", "：", "？", "！"})
+word_sentence_list = ws(all_news_li2, recommend_dictionary=dictionary,
+                        segment_delimiter_set={",", "。", ":", "?", "!", ";", "，", "：", "？", "！", "；"})
 # print(len(word_sentence_list))
 # print(word_sentence_list)
 pos_sentence_list = pos(word_sentence_list)
 entity_sentence_list = ner(word_sentence_list, pos_sentence_list)
 # TF (1~XXXX)=>min max normalize, NER(1/0 or 1~XX)=>set 1, TF-IDF (0~1)
 temp = []
-bad_list = ["<br>","br","BR","<BR>","，"]
+bad_list = ["<br>", "br", "BR", "<BR>", "，", "【", "】", "╱", "▲", "▼", "&amp;amp;amp;amp;amp;lt;br",
+            "&amp;amp;amp;amp;amp;gt", "amp", "lt", "br&", "gt", "&amp", "[", "]"]
 for w_s_l in word_sentence_list:
     t = [x for x in w_s_l if x not in bad_list]
     temp.append(t)
 word_sentence_list = temp
+
+tokenize_end = time.time() - tokenize_start
+algo_start = time.time()
 # by news TF
 TF_news_li = []
 for i, news_toks in enumerate(word_sentence_list):
@@ -97,7 +111,7 @@ all_corpus = []
 for sentence in word_sentence_list:
     all_corpus.append(" ".join(sentence))
 # print(all_corpus[0])
-vectoerizer = CountVectorizer(min_df=1, max_df=1.0, token_pattern='\\b\\w+\\b')
+vectoerizer = CountVectorizer(min_df=2, max_df=0.99, token_pattern='\\b\\w+\\b')
 vectoerizer.fit(all_corpus)
 X = vectoerizer.transform(all_corpus)
 tfidf_transformer = TfidfTransformer()
@@ -171,7 +185,13 @@ for title_str, news_str, news_tok_li, com_df in zip(all_title_li, all_news_li, w
     df = df.append(temp, ignore_index=True)
 df.to_excel(result_path)
 print(df)
-
+algo_end = time.time() - algo_start
+print("Model Load Time:", '{:02d}:{:02d}:{:02d}'.format(load_end // 3600, (load_end % 3600 // 60), load_end % 60))
+print("DL Tokenize Time:",
+      '{:02d}:{:02d}:{:02d}'.format(tokenize_end // 3600, (tokenize_end % 3600 // 60), tokenize_end % 60))
+print("KeyWord Algorithm Time:",
+      '{:02d}:{:02d}:{:02d}'.format(algo_end // 3600, (algo_end % 3600 // 60), algo_end % 60))
+# 2628 Foxconn
 '''
 # for ALL NER
 for i, en_sentence in enumerate(entity_sentence_list):  # all
